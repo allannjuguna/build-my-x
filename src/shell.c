74854987659edaae991b881fc15be74e6e48eabd
xubzero <allannjugush@gmail.com>
2023-06-27 10:56:10 +0300

    Added a while loop and chdir functionality to the shell

--- src/shell.c
+++ src/shell.c
@@ -10,49 +10,70 @@
 * They will also allow independent commands to have their own process blocks (isolation) thus helping in fault torelance
 */
 
-
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-/* Creating a simple fork */
-int main() {
-	printf("This is the main function of the program\n");
+#define SHELLNAME "unixsh"
+#define DELIMETER " "
+#define BUFFER 100
 
-	int stat_loc;
+int main(int argc,char **argv){
+	char prompt[BUFFER];
+	ssize_t stat_loc;
+	pid_t child_fork=0;
+	char *command[10];
 
+while (1){
+	/* Accepting the user input */
+	printf("\n%s > ",SHELLNAME);
+	fgets(prompt,BUFFER,stdin);
 
-	pid_t child_pid=fork();
-	if (child_pid == 0){ /*Child process*/
-		printf("Child :: Current PID %d and Child PID : %d\n", getpid(),child_pid);
-		sleep(2);
-	}else{ /*Parent process*/
-		// We want to wait for the child process
-		pid_t wait_result=waitpid(child_pid,&stat_loc,WUNTRACED);
-		printf("Parent :: Current PID %d and Child PID : %d\n", getpid(),child_pid);
+	/* Cleaning the user input */
+	strncpy(prompt,strtok(prompt,"\n"),BUFFER);
 
-        if (WIFEXITED(stat_loc)) {
-            // Child process exited normally
-            int exit_status = WEXITSTATUS(stat_loc);
-            printf("Child process %d terminated with exit status %d\n", wait_result, exit_status);
-        } else if (WIFSIGNALED(stat_loc)) {
-            // Child process terminated by a signal
-            int signal_number = WTERMSIG(stat_loc);
-            printf("Child process %d terminated by signal %d\n", wait_result, signal_number);
-        }
-
-
-
+	if (strstr(prompt,"exit") != NULL){
+		fprintf(stderr, "Exit invoked. Closing the shell\n");
+		exit(EXIT_SUCCESS);
 	}
 
+	/*Splitting the input string into an array*/
+	int index=0;
+	char *parsed = strtok(prompt, DELIMETER);
+	while (parsed != NULL) {
+	    command[index] = parsed;
+	    index++;
+	    parsed = strtok(NULL, DELIMETER);
+	}
+	command[index] = NULL;
+
+	if (strcmp(command[0],"cd") == 0){
+		if(chdir(command[1]) < 0){
+			fprintf(stderr, "An error occurred while changing directory\n");
+		}else{
+			printf("Changed dir to %s\n",command[1]);
+		}
+		continue;
+	}
+
+	child_fork=fork();
+	if (child_fork == 0){
+		// This is the child process
+	    ssize_t retcode=execvp(command[0],command);	
+	    if (retcode < 0){
+	    	fprintf(stderr, "An error occurred while running command '%s'\n", prompt);
+	    	exit(EXIT_FAILURE);
+	    }
+		// printf("Child process spawned with pid of %d. Return of exec is %d\n",getpid(),retcode);
+	}else{ 
+		// This is the parent process
+		// printf("Main process is running with pid of %d and with a child with pid %d\n",getpid(), child_fork );
+		waitpid(child_fork,&stat_loc,0);
+	}
+
+}
 	return 0;
 }
 
-
-
-/*This is the main function of the program
-Child :: Current PID 42491 and Child PID : 0
-Parent :: Current PID 42490 and Child PID : 42491*/
